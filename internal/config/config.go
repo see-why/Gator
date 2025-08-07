100efc19019992df6a89d241bce9a516ec924f52
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-08-06 23:33:17 -0400

    add config.go file with functionality to read and write configuration

---
+++ internal/config/config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const configFileName = ".gatorconfig.json"
+
+// Config represents the JSON file structure
+type Config struct {
+	DbURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name,omitempty"`
+}
+
+// Read reads the JSON file found at ~/.gatorconfig.json and returns a Config struct
+func Read() (Config, error) {
+	var cfg Config
+
+	configPath, err := getConfigFilePath()
+	if err != nil {
+		return cfg, err
+	}
+
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return cfg, err
+	}
+
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		return cfg, err
+	}
+
+	return cfg, nil
+}
+
+// SetUser sets the current_user_name field and writes the config struct to the JSON file
+func (cfg *Config) SetUser(username string) error {
+	cfg.CurrentUserName = username
+	return write(*cfg)
+}
+
+// getConfigFilePath returns the full path to the config file
+func getConfigFilePath() (string, error) {
+	return configFileName, nil
+}
+
+// write writes the config struct to the JSON file
+func write(cfg Config) error {
+	configPath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	data, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(configPath, data, 0644)
+}
