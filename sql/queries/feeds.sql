9ac13e9bd4f6035d2cda65f11a920675a813fc5c
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-08-13 23:11:34 -0400

    feat: add SQL query to retrieve feeds along with associated user names

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -9,3 +9,16 @@ VALUES (
     $6
 )
 RETURNING *;
+
+-- name: GetFeedsWithUsers :many
+SELECT 
+    f.id,
+    f.created_at,
+    f.updated_at,
+    f.name,
+    f.url,
+    f.user_id,
+    u.name as user_name
+FROM feeds f
+JOIN users u ON f.user_id = u.id
+ORDER BY f.created_at DESC;
