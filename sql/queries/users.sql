0b3ae6024e8d3551adec669c94c68c015a4ae87e
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-09-13 10:00:00 -0400

    feat: add api_key column to CreateUser query and update related queries

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,15 +1,22 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, name)
+INSERT INTO users (id, created_at, updated_at, name, api_key)
 VALUES (
     $1,
     $2,
     $3,
-    $4
+    $4,
+    $5
 )
 RETURNING *;
 
 -- name: GetUser :one
 SELECT * FROM users WHERE name = $1;
+
+-- name: GetUserByAPIKey :one
+SELECT * FROM users WHERE api_key = $1;
+
+-- name: UpdateUserAPIKey :exec
+UPDATE users SET api_key = $1, updated_at = CURRENT_TIMESTAMP WHERE id = $2;
 
 -- name: GetUsers :many
 SELECT * FROM users;
